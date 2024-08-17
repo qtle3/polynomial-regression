11f9143a06b20058397ebc58ba760bed26035c15
Quocviet Le <quocvietlet@gmail.com>
2024-08-16 23:44:58 -0500

    import machine learning libraries

---
+++ polynomial_regression.py
@@ -0,0 +1,10 @@
+# import libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# import machine learning libraries
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
